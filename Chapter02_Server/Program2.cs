de1c11b293a8cc66ec2777dddabfd632293d5757
김주원 <juwon98999@naver.com>
2022-04-21 00:57:06 +0900

    초기 커밋.

---
+++ Chapter02_Server/Program2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using ChatServer_Mutex;
+using System.Collections.Generic;
+
+
+namespace Chapter02_Server
+{
+    class MyTcpListener
+    {
+
+        Byte[] bytes = new Byte[256];
+        TcpListener server = null;
+        bool ClientConnect = false;
+        String Read_data = null;
+        NetworkStream stream;
+
+        static void ServerStart()
+        {
+            MyTcpListener ML = new MyTcpListener();
+            LinkedList<String> Server_list = new LinkedList<String>();
+            Server_list.AddLast("Wainting Connection...");
+
+            foreach (var chat in Server_list)
+            {
+                Console.WriteLine(chat);
+            }
+
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                ML.server = new TcpListener(localAddr, port);
+                ML.server.Start();
+                ML.ClientConnect = true;
+
+
+                TcpClient client = ML.server.AcceptTcpClient();
+                Server_list.AddLast("'주' 님께서 연결되었습니다.");
+
+
+                int i;
+
+                while (true)
+                {
+
+                    ML.Read_data = null;
+                    ML.stream = client.GetStream();
+
+                    Console.Clear();
+                    foreach (var chat in Server_list)
+                    {
+                        Console.WriteLine(chat);
+                    }
+ 
+
+                    ConsoleKeyInfo WirteKey = Console.ReadKey(true);
+
+                    if (WirteKey.Key == ConsoleKey.T)
+                    {
+                        Console.WriteLine("메세지를 입력해주세요.");
+
+                        String Send_message = Console.ReadLine();
+                        Byte[] send_data = System.Text.Encoding.Default.GetBytes(Send_message);
+                        ML.stream.Write(send_data, 0, send_data.Length);
+                        Console.WriteLine($"주[] {Send_message}");
+                    }
+
+                    if (ML.ClientConnect)
+                    {
+                        if ((i = ML.stream.Read(ML.bytes, 0, ML.bytes.Length)) != 0)
+                        {
+                            ML.Read_data = System.Text.Encoding.Default.GetString(ML.bytes, 0, i);
+                            Server_list.AddLast($"[수] {ML.Read_data}");
+                        }
+                    }
+
+                    byte[] msg = System.Text.Encoding.Default.GetBytes(ML.Read_data);
+
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread Server = new Thread(new ThreadStart(ServerStart));
+            Server.Start();
+
+        }
+    }
+}
---
+++ ChatServer_Mutex/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace ChatServer_Mutex
+{
+
+    public class MyTcpClient
+    {
+
+        bool isConnect = false;
+        Byte[] data = new Byte[256];
+        static string server = "127.0.0.1";
+        Int32 port = 13000;
+        TcpClient client = new TcpClient();
+        NetworkStream stream;
+        String message;
+
+        static void ClientStart()
+        {
+            MyTcpClient Mc = new MyTcpClient();
+            LinkedList<String> Client_list = new LinkedList<String>();
+
+            String IpInput = Console.ReadLine();
+            if(IpInput == $"/c {server}")
+            {
+                Client_list.AddLast($"{MyTcpClient.server}에 접속 시도중...");
+                Client_list.AddLast("'수'님이 접속하셨습니다.");
+
+                foreach (var chat in Client_list)
+                {
+                    Console.WriteLine(chat);
+                }
+            }
+
+            try
+            {
+
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.%
+
+                Mc.client = new TcpClient(server, Mc.port);
+                Mc.stream = Mc.client.GetStream();
+
+                while (true)
+                {
+
+                    Mc.isConnect = true;
+
+                    Console.Clear();
+                    foreach (var chat in Client_list)
+                    {
+                        Console.WriteLine(chat);
+                    }
+
+
+                    ConsoleKeyInfo WirteKey = Console.ReadKey(true);
+
+                    if (WirteKey.Key == ConsoleKey.T)
+                    {
+                        Console.WriteLine("메세지를 입력해주세요.");
+
+                        Mc.message = Console.ReadLine();
+
+                        if (Mc.message == "/q")
+                        {
+                            Mc.stream.Close();
+                            Mc.client.Close();
+                            Console.WriteLine("프로그램이 종료 됩니다.");
+                            Func();
+                        }
+
+                        Mc.data = System.Text.Encoding.Default.GetBytes(Mc.message);
+                        Mc.stream.Write(Mc.data, 0, Mc.data.Length);
+                        Client_list.AddLast($"수[] {Mc.message}");
+                    }
+
+                    if (Mc.isConnect == false)
+                    {
+                        String Read_Message = null;
+                        Mc.data = new Byte[256];
+                        Read_Message = null;
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = Mc.stream.Read(Mc.data, 0, Mc.data.Length);
+                        Read_Message = System.Text.Encoding.Default.GetString(Mc.data, 0, bytes);
+                        Client_list.AddLast($"주[] {Read_Message}");
+                    }
+
+                }
+                
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+
+        }
+
+        static void Func()
+        {
+            Environment.Exit(0);
+        }
+
+        public static void Main()
+        {
+            Thread Client = new Thread(new ThreadStart(ClientStart));
+            Client.Start();
+        }
+    }
+}
